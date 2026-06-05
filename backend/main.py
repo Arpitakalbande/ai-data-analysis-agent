55ae632521b83cd049b5d057b097ebc529d9d4d3
Arpit Banait <arpit@example.com>
2026-06-05 12:50:22 +0530

    updated allow credentials

--- backend/main.py
+++ backend/main.py
@@ -7,7 +7,7 @@ app = FastAPI(title="Data Analysis Agent - Minimal API")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:5173", "http://127.0.0.1:5173","https://data-analytics-agent-psi.vercel.app"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
