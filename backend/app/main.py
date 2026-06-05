32368a63287c75061de95e40214824a36c77a9d6
Arpit Banait <arpit@example.com>
2026-06-05 13:48:04 +0530

    updated

--- backend/app/main.py
+++ backend/app/main.py
@@ -11,11 +11,7 @@ init_db()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-        "https://data-analytics-agent-psi.vercel.app"
-    ],
+    allow_origins=["*"],
     allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
